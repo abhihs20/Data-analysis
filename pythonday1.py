f47fd2c5a55bcb8732314bcd026076f30e5893f5
abhihs20 <hsabhishek43@gmail.com>
2025-07-19 13:17:15 +0530

    Add files via upload
    I am very exited to share this , This is my first Data analysis class. In this class i can learn python python is a dynamic language use in world wide

---
+++ pythonday1.py
@@ -0,0 +1,119 @@
+# print("Hello") # this is the First line of Code in my Python Class
+
+# print(12)
+
+# print("Manoj")
+
+# Inbuilt Functions : Predefined Functions
+# print : to print the o/p
+# input : to take the Input from the User
+
+ #print("My Name is Manoj")
+
+# input("What's Your Name ?")
+
+# Variables :- they are the Storage CLasses to Store the Data. and to access that later
+
+# Rules for Naming the Variable.
+
+# Should start with Alphabets
+# special Characters are not allowed except _(Underscore)
+# White space is Not Allowed
+# camel casing is Allowed (Helps for Code readability)
+# it's case sensitive
+
+# myName = "Manoj"
+# print(myName)
+
+# We can store any kind of Data in variables
+# Data : any information 
+# name , age, phNo, Address, yes or no (True or False)
+
+# Concatenation : Adding more than 1 value into a Single Str ing
+# print("My Name is " +name+ "My Address is " + address ) # + is Called as Unary Operator
+
+# f string
+
+ #print(f"My name is {name} and my Age is {age} and I'm From {address} and My Marital status is {isMarried}")  
+
+
+# Taking User Input 
+
+# name = input("Enter your name")
+
+# print(f"my Name is {name}")
+
+
+# Data Types : Type of the Data 
+# Numbers (int,float)
+# Strings (Any Data which is enclosed in single quotes or double quotes)
+# Boolean : True or False
+# undefined 
+# null
+
+#name = "manoj" #String
+#age = 25 #Number
+# address = "Mysore" #String
+# isMarried = False #Boolean
+# weight = 62.5
+
+# type : to Check the Type of the Data which is Assigned to the Variable
+
+#print(type(name))
+#print(type(age))
+# print(type(weight))
+# print(type(isMarried))
+
+
+#list1=[12,55,6,88,540,100]
+#print(list1)
+#list1.append(80)
+#print(list1)
+#list1.count(12)
+#list1.insert(3,25)
+#list1.copy
+#list1.extend([200,300])
+#list1.pop(2)
+#list1.remove(88)
+#list1.reverse()
+#list1.sort()
+#list1.clear()
+#print(list1)
+
+
+
+#list2=[22,67,88,44,33,100,55]
+#print(list2)
+# list2.append(200)
+print("appendend list",list2)
+list2.extend([500])
+print("extendend list",list2)
+list2.insert(3,444)
+print("inserted list",list2)
+list2.pop()
+print("poped list",list2)
+list2.remove(67)
+print("removed list",list2)
+list2.reverse()
+print("reversed list",list2)
+list2.sort()
+print("sorted list",list2)
+list2.count(100)
+print(list2)
+list2.copy()
+print(list2)
+list2.clear()
+print(list2)
+
+
+
+name=input("enter your name")
+address=input("enter your address")
+
+print(f" my name is {name} my address is {address}")
+
+
+
+list3=["abhi","abhi"]
+print(list3)
+
