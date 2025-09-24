f8af8e2dcf680b24ae9cf6a885da887e811d1425
abhihs20 <hsabhishek43@gmail.com>
2025-09-24 17:43:28 +0530

    Add files via upload
    python day 4 class

---
+++ day4.py
@@ -0,0 +1,42 @@
+# Dictionary : to store and manage the Data in a key and value pairs.
+# unordered collection of Data
+# Mutables
+# value : it defines the Data
+# key : it represents the Value which is Stored in Value
+
+# syntax : rule
+# creating using : {key : value}
+
+# info = {
+#     "name":"Manoj", #key : value pair
+#     "age" : 25,
+#     "place": "Mysore",
+#     "job" : "Web Dev",
+#     "isMarried" : False,
+#     "company" : {
+#         "cName" : "Acube",
+#         "cLocation" : "Mysore"
+#     } #nested key-value pairs
+# }
+
+# print(info)
+
+# values can be Accessed using key 
+
+# print(info.values()) # to get the values from the dictionary
+
+# print(info.keys()) # to get the keys from the dictionary
+
+# print(info.get('name')) # to get the Value from the Particular keys
+# print(info.get('company'))
+
+# info.pop('company') # to delete the Particular key value pairs
+
+# print(info)
+
+# info.update({"name":"Shreyas","age":13}) 
+
+# print(info)
+
+# print(info.get('cName')) 
+# How to Access the key and value pairs in nested Dictionary ?
