69ae29525a5c65bc910ee01594d580c090b47d41
abhihs20 <hsabhishek43@gmail.com>
2025-09-24 17:42:34 +0530

    Add files via upload
    python day 3 class

---
+++ day3.py
@@ -0,0 +1,23 @@
+# Tuple : used to store the diff data of Diff Data Types
+# It Does Allow Duplicates
+# creating using (common Brackets)
+# Elements can be Accessed using index Values
+# TUPles are Immutables
+
+# myTuple = ('Manoj',"Acube",2024,2024,"Manoj","Acube")
+
+# print(myTuple)
+
+# print(myTuple[5])
+
+# myTuple[0] = 'Vivek'
+# print(myTuple)
+
+# Count : to count the no of Occurences
+# print(myTuple.count("Manoj"))
+
+# Changing Tuple into List
+
+# tuple_to_list = list(myTuple)
+
+# print(tuple_to_list)
