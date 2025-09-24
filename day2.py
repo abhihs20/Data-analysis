ae1674cfefd6aa2720a9b6dce39938c93a2c011b
abhihs20 <hsabhishek43@gmail.com>
2025-09-24 17:21:44 +0530

    Add files via upload
    python day2 class

---
+++ day2.py
@@ -0,0 +1,46 @@
+# Data Structure : the Way of Organising the Data, in a Structured Manner
+
+# 1. List
+# 2. Tuple
+# 3. Sets
+# 4. Dictionary
+
+# 1. List : Collection of Non-Homogeneous Elements
+# Collection of elements of Diff data Types
+# Define : [square brackets]
+# list is Index based (position starts from 0)
+# lists are Mutable : we can the Values
+# by using index values
+
+# list1 = ["Manoj",20, 30,"Mysore", True, 3.14, False]
+
+# print(list1[3])
+
+# list1[3] = "Hassan"
+# print(list1)
+
+# Methods : What all the Operations can be Performed withrespect list
+# numbers = [10,5,2,1,100,1000]
+# # list1.sort()
+# # numbers.sort()
+# numbers.append(3) # adding elements at the last
+# numbers.reverse()
+# numbers.clear()
+# print(numbers)
+
+# List Allwos Duplicate Items
+
+# list1 = ['A',"A"]
+# print(list1)
+
+
+# list1 = ["Manoj",20, 30,"Mysore"]
+# print(list1.index("Manoj"))
+# list2 = [True,3.14,False]
+
+# extend : to extend the one list with another
+
+# list1.extend(list2)
+
+# # print(list1.extend(list2))
+# print(list1)
