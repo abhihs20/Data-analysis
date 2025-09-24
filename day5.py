3bb2bd93034f05b4f8e10668776f4e1e1cf33119
abhihs20 <hsabhishek43@gmail.com>
2025-09-24 17:44:38 +0530

    Add files via upload
    python day 5 class

---
+++ day5.py
@@ -0,0 +1,31 @@
+# Sets : collection of Data of diff Data type
+# immutables
+# it doesn't allow duplicate Values
+# Unordered
+
+# defined using : {value1, value2,value3}
+
+# mySet = {1,2,3}
+
+# mySet2 = {4,5,6}
+
+# print(mySet)
+# mySet.update("manoj")
+# mySet.update("Mysore") # to Separate the Characters
+# print(mySet)
+
+# print(mySet.union(mySet2))  # to print all the elements from the sets
+
+# print(mySet2.intersection(mySet)) # to get the Common values in both the sets
+
+
+# print(mySet.isdisjoint(mySet2))
+
+# list1 = ['Manoj','Manoj',"Mysore","Mysore"]
+
+# convert_list_to_Sets = set(list1)
+
+# convert_set_to_List = list(convert_list_to_Sets)
+
+# # print(convert_list_to_Sets)
+# print(convert_set_to_List)
