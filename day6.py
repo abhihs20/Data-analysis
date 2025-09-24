830eb205435218ad26980630749088552786e5ef
abhihs20 <hsabhishek43@gmail.com>
2025-09-24 17:45:11 +0530

    Add files via upload
    python day 6 class

---
+++ day6.py
@@ -0,0 +1,81 @@
+# Condtions :
+# if : if the condtion is True
+# if else : if the condition is False then the else block will execute
+# elif (Nested If) : to Handle the Multiple Conditions
+
+# if:
+# Syntax :
+
+# age = 19
+
+# if(age>=18):
+#     print("You can Vote")
+
+# else:
+#     print("You cannot Vote")
+
+
+# elif : to Handle the Multiple COnditions
+
+# marks = int(input("Enter Your Marks :  "))
+
+# if(marks>=85):
+#     print("A")
+
+# elif(marks>=65):
+#     print("B")
+
+# elif(marks>=35):
+#     print("C")
+
+# else:
+#     print("F")
+
+
+# Logical Operators with COndtions
+# and , or, not
+
+# Driving Criteria using conditions and Logics
+
+# haveDL = True
+# haveGoodVision = False
+
+# if(not haveDL or not haveGoodVision):
+#     print("You can Drive")
+
+# else:
+#     print("You cannot Drive")
+
+
+# SpeedLimit Checking on Fine
+
+# vehicleSpeed = 260
+
+# if(vehicleSpeed <= 100):
+#     print("No Fine")
+
+# elif(vehicleSpeed > 100 and vehicleSpeed < 150):
+#     print("1000 rupees fine😥")
+
+# elif(vehicleSpeed >150 and vehicleSpeed < 200):
+#     print("2000 rs Fine")
+
+# else:
+#     print("Seize the Vehicle")
+
+
+# Login Functionality 
+# userName
+# Password
+
+# userName = 'Manoj@20'
+# password = 'ManojAcube'
+
+# userNameInput = input("Enter userName :  ")
+# userPasswordInput = input("Enter the Password :  ")
+
+# if(userNameInput == 'Manoj@20' and userPasswordInput == 'ManojAcube'):
+#     print("Login Successfull..!")
+
+# else:
+#     print("Invalid credentials")
