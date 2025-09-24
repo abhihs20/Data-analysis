e369d835b497a8f1c9b3727898d1e73f671077f6
abhihs20 <hsabhishek43@gmail.com>
2025-09-24 17:46:44 +0530

    Add files via upload
    python day 7 class

---
+++ day7.py
@@ -0,0 +1,40 @@
+# Function: Named set of Instructions to perfotm the Specific tasks
+# re-usable blocks
+
+# syntax : def fnName(): 
+                # code to be executed
+            # fnName()
+
+# def greet():
+#     print("Hello Good Morning")
+
+# greet() #function calling using Function name
+# greet()
+# greet()
+# greet()
+# greet()
+# greet()
+
+# Fn Parameters : they are the storage classes, which acts as a Variables to store the Data.
+# fn Arumnets : Values which passed onto the Parameters
+# def greet(name,msg):
+#     print(f"Hello {name}, {msg}")
+
+# # greet("Manoj")
+# # greet("Vivek")
+# # greet()
+
+# greet("Manoj","Uta Aytha..?")
+# greet("Shreyas","Nidde Madbeda")
+
+# def addition(a,b):
+#     return a + b  # return : used to get the values from the Fucntion (it returns the Value)
+
+# # result = addition(10,20)
+
+# aInput = int(input("Enter the Value of a :  ")) 
+# bInput = int(input("Enter the Value of b :  ")) 
+
+
+# result = addition(aInput,bInput)
+# print(result)
